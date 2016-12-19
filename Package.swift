5586cf7007556e5785597260f0078edc1a80043a
phil <philiperickson@gmail.com>
2016-12-18 19:04:05 -0700

    Created single package for system library dependencies

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Nifty",
     dependencies: [
-        .Package(url: "https://github.com/nifty-swift/CLapacke.git", majorVersion: 1),
-        .Package(url: "https://github.com/nifty-swift/CBlas.git", majorVersion: 1)
+        .Package(url: "https://github.com/nifty-swift/Nifty-libs.git", majorVersion: 1),
     ]
 )
