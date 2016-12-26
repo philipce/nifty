a16b97a56d220ba6f8c50f4616e06855144b1750
pce <philiperickson@gmail.com>
2016-12-26 14:43:56 -0700

    Removed meaningless prints

--- Tests/NiftyTests/lu_test.swift
+++ Tests/NiftyTests/lu_test.swift
@@ -23,16 +23,19 @@ class lu_test: XCTestCase
                 [4,5,6],
                 [7,8,0]], name: "A", showName: true)
 
-        let (L, U, P) = lu(A)
-
+        print("Calling lu(A) = (L,U,P):")
+        let (L, U, P) = lu(A)        
         print(A, terminator: "\n\n")
         print(L, terminator: "\n\n")
         print(U, terminator: "\n\n")
         print(P, terminator: "\n\n")
-        print(P*L, terminator: "\n\n")
-        print((P*L)*U, terminator: "\n\n")
         print(P*A, terminator: "\n\n")
         print(L*U, terminator: "\n\n")
+
+        print("Calling lu(A) = (L,U):")
+        let (L2, U2) = lu(A)
+        print(L2, terminator: "\n\n")
+        print(U2, terminator: "\n\n")
 
         print("\n\n")
     }
