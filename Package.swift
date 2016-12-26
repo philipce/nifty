5ab47072046c4ed0cc36d27d16362e527c6b3143
pce <philiperickson@gmail.com>
2016-12-25 18:40:56 -0700

    Add framework for test results

--- Package.swift
+++ Package.swift
@@ -4,5 +4,9 @@ let package = Package(
     name: "Nifty",
     dependencies: [
         .Package(url: "https://github.com/nifty-swift/Nifty-libs.git", majorVersion: 1),
+    ],
+    exclude: [
+    	"Tests/KnownResults",
     ]
+
 )
