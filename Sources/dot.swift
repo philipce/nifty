09bcb25d4204f82c7b4df149b2829ce256333374
phil <philiperickson@gmail.com>
2016-12-30 23:40:08 -0700

    Upate comment

--- Sources/dot.swift
+++ Sources/dot.swift
@@ -4,7 +4,7 @@
  *  This file defines vector dot product functionality. 
  *
  *  Author: Philip Erickson
- *  Creation Date: 14 Aug 2016
+ *  Creation Date: 29 Dec 2016
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at
