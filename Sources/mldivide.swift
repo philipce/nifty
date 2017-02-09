901aa7c6d2966d48413f8ae2baf4c22210e9a6f7
phil <philiperickson@gmail.com>
2017-02-08 22:30:51 -0700

    Added overdetermined test
    Took a stab at redoing the overdetermined case of mldivide with Accelerate, but I was on a Linux box so I couldn't run it... hence it will probably not be correct

--- Sources/mldivide.swift
+++ Sources/mldivide.swift
@@ -69,28 +69,26 @@ public func mldivide(_ A: Matrix<Double>, _ B: Matrix<Double>) -> Matrix<Double>
         // TODO: find better way to resolve the difference between clapack used by Accelerate and LAPACKE
         #if NIFTY_XCODE_BUILD
             
-        // Everything is column-major order when using Accelerate 
-        let At = transpose(A)
-        let Bt = transpose(B)
-        var at = At.data  
-        var bt = Bt.data
-        lda = Int32(A.size[0])
-        ldb = Int32(B.size[0])
-        dgesv_(&n, &nrhs, &at, &lda, &ipiv, &bt, &ldb, &info)
-        b = transpose(Matrix(Bt.size, bt)).data 
-        
+            // Everything is column-major order when using Accelerate 
+            let At = transpose(A)
+            let Bt = transpose(B)
+            var at = At.data  
+            var bt = Bt.data
+            lda = Int32(A.size[0])
+            ldb = Int32(B.size[0])
+            dgesv_(&n, &nrhs, &at, &lda, &ipiv, &bt, &ldb, &info)
+            b = transpose(Matrix(Bt.size, bt)).data 
+            
         #else
         
-        info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, nrhs, &a, lda, &ipiv, &b, ldb)
+           info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, nrhs, &a, lda, &ipiv, &b, ldb)
             
         #endif
         
         precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
         precondition(info == 0, "Cannot solve singularity")
         
-        return Matrix(Int(n), Int(nrhs), b, name: newName, showName: A.showName || B.showName)
-            
-        
+        return Matrix(Int(n), Int(nrhs), b, name: newName, showName: A.showName || B.showName)                    
     }
 
     // otherwise return least-squares solution
@@ -105,27 +103,49 @@ public func mldivide(_ A: Matrix<Double>, _ B: Matrix<Double>) -> Matrix<Double>
             var nrhs = Int32(B.size[1])
             var a = A.data
             var b = B.data
-
-            // The leading dimension equals the number of elements in the major dimension. In this 
-            // case, we are doing row-major so lda is the number of columns in A, e.g.
-            var lda = Int32(A.size[1])
-            var ldb = Int32(B.size[1])
-            
             var info = Int32(0)
             
             // TODO: find better way to resolve the difference between clapack used by Accelerate and LAPACKE
             #if NIFTY_XCODE_BUILD
-            // FIXME: pretty sure this will crash, same issues as the underdetermined system probably...
-            // Need to add test case that hits this usage and then fix it
-            var trans: Int8 = 84 // ascii 'T'
-            let mn = min(m, n) 
-            var lwork = mn + max(mn, nrhs) // TODO: revisit this for optimal performance... see lapack docs 
-            var work = Array<Double>(repeating: 0, count: Int(lwork))
-            dgels_(&trans, &m, &n, &nrhs, &a, &lda, &b, &ldb, &work, &lwork, &info)            
-            b = transpose(Matrix(B.size, b)).data // FIXME: I'm assuming I need to transpose this on the way out... is that right?
-            #else
-            let trans: Int8 = 78 // ascii 'N'
-            info = LAPACKE_dgels(LAPACK_ROW_MAJOR, trans, m, n, nrhs, &a, lda, &b, ldb)
+            
+                // Everything is column-major order when using Accelerate 
+                a = transpose(A).data
+                let Bt = transpose(B)
+                b[0..<Bt.count] = Bt.data[0..<Bt.count]                  
+                var lda = Int32(A.size[0])
+                var ldb = max(m,n) // FIXME: seems like this should be Int32(B.size[0]), why is it max(1,m,n) in docs?
+                                
+                // TODO: redo this with 'T' to save an extra transpose
+                var trans: Int8 = 78 // ascii 'n' (ascii 'T' = 84)    
+                let mn = min(m, n) 
+                var lwork = mn + max(mn, nrhs) // TODO: revisit this for optimal performance... see lapack docs 
+                var work = Array<Double>(repeating: 0, count: Int(lwork))
+                dgels_(&trans, &m, &n, &nrhs, &a, &lda, &b, &ldb, &work, &lwork, &info)    
+                
+                print("b=\(b)")
+                                    
+                precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
+                if info != 0 { print("Warning: Matrix does not have full rank") }   
+                
+                let x = Array(b[0..<Int(n*nrhs)]) 
+
+                let Xt = Matrix(Int(nrhs), Int(n), x, name: newName, showName: A.showName || B.showName)
+                var X = transpose(Xt)
+                X.name = Xt.name
+                X.showName = Xt.showName
+
+                return X
+
+            #else              
+
+                // The leading dimension equals the number of elements in the major dimension. In this 
+                // case, we are doing row-major so lda is the number of columns in A, e.g.
+                let lda = Int32(A.size[1])
+                let ldb = Int32(B.size[1])
+
+                let trans: Int8 = 78 // ascii 'N'
+                info = LAPACKE_dgels(LAPACK_ROW_MAJOR, trans, m, n, nrhs, &a, lda, &b, ldb)
+            
             #endif
                 
             precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
@@ -139,8 +159,6 @@ public func mldivide(_ A: Matrix<Double>, _ B: Matrix<Double>) -> Matrix<Double>
         // underdetermined system
         else
         {
-            print(A)
-            print(B)
             
             var nrhs = Int32(B.size[1])
             var a = A.data
@@ -148,58 +166,57 @@ public func mldivide(_ A: Matrix<Double>, _ B: Matrix<Double>) -> Matrix<Double>
             // solution is larger than B so extra space must be added
             var b = Array<Double>(repeating: 0, count: Int(n*nrhs))
                   
-
             var info = Int32(0)
             
             // TODO: find better way to resolve the difference between clapack used by Accelerate and LAPACKE
             #if NIFTY_XCODE_BUILD
                                                 
-            // Everything is column-major order when using Accelerate 
-            a = transpose(A).data
-            let Bt = transpose(B)
-            b[0..<Bt.count] = Bt.data[0..<Bt.count]                  
-            var lda = Int32(A.size[0])
-            var ldb = max(m,n) // FIXME: seems like this should be Int32(B.size[0]), why is it max(1,m,n) in docs?
-                            
-            var trans: Int8 = 78 // ascii 'n' (ascii 'T' = 84)	
-            let mn = min(m, n) 
-            var lwork = mn + max(mn, nrhs) // TODO: revisit this for optimal performance... see lapack docs 
-            var work = Array<Double>(repeating: 0, count: Int(lwork))
-            dgels_(&trans, &m, &n, &nrhs, &a, &lda, &b, &ldb, &work, &lwork, &info)    
-            
-            print("b=\(b)")
+                // Everything is column-major order when using Accelerate 
+                a = transpose(A).data
+                let Bt = transpose(B)
+                b[0..<Bt.count] = Bt.data[0..<Bt.count]                  
+                var lda = Int32(A.size[0])
+                var ldb = max(m,n) // FIXME: seems like this should be Int32(B.size[0]), why is it max(1,m,n) in docs?
                                 
-            precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
-            if info != 0 { print("Warning: Matrix does not have full rank") }   
-            
-            let x = Array(b[0..<Int(n*nrhs)]) 
+                // TODO: redo this with 'T' to save an extra transpose
+                var trans: Int8 = 78 // ascii 'n' (ascii 'T' = 84)	
+                let mn = min(m, n) 
+                var lwork = mn + max(mn, nrhs) // TODO: revisit this for optimal performance... see lapack docs 
+                var work = Array<Double>(repeating: 0, count: Int(lwork))
+                dgels_(&trans, &m, &n, &nrhs, &a, &lda, &b, &ldb, &work, &lwork, &info)    
+                
+                print("b=\(b)")
+                                    
+                precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
+                if info != 0 { print("Warning: Matrix does not have full rank") }   
+                
+                let x = Array(b[0..<Int(n*nrhs)]) 
 
-            let Xt = Matrix(Int(nrhs), Int(n), x, name: newName, showName: A.showName || B.showName)
-            var X = transpose(Xt)
-            X.name = Xt.name
-            X.showName = Xt.showName
+                let Xt = Matrix(Int(nrhs), Int(n), x, name: newName, showName: A.showName || B.showName)
+                var X = transpose(Xt)
+                X.name = Xt.name
+                X.showName = Xt.showName
 
-            return X
+                return X
                 
             #else
                 
-            // The leading dimension equals the number of elements in the major dimension. In this 
-            // case, we are doing row-major so lda is the number of columns in A, e.g.
-            var lda = Int32(A.size[1]) 
-            var ldb = Int32(B.size[1])    
+                // The leading dimension equals the number of elements in the major dimension. In this 
+                // case, we are doing row-major so lda is the number of columns in A, e.g.
+                let lda = Int32(A.size[1]) 
+                let ldb = Int32(B.size[1])    
+                    
+                b[0..<B.count] = B.data[0..<B.count]      
+                                    
+                let trans: Int8 = 78 // ascii 'N'            
+                info = LAPACKE_dgels(LAPACK_ROW_MAJOR, trans, m, n, nrhs, &a, lda, &b, ldb)
+                    
+                precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
+                if info != 0 { print("Warning: Matrix does not have full rank") }   
                 
-            b[0..<B.count] = B.data[0..<B.count]      
+                let x = Array(b[0..<Int(n*nrhs)])          
                 
-                
-            let trans: Int8 = 78 // ascii 'N'            
-            info = LAPACKE_dgels(LAPACK_ROW_MAJOR, trans, m, n, nrhs, &a, lda, &b, ldb)
-                
-            precondition(info >= 0, "Illegal value in LAPACK argument \(-1*info)")
-            if info != 0 { print("Warning: Matrix does not have full rank") }   
-            
-            let x = Array(b[0..<Int(n*nrhs)])          
-            
-            return Matrix(Int(n), Int(nrhs), x, name: newName, showName: A.showName || B.showName)
+                return Matrix(Int(n), Int(nrhs), x, name: newName, showName: A.showName || B.showName)
 
             #endif  
         }
--- Tests/NiftyTests/mldivide_test.swift
+++ Tests/NiftyTests/mldivide_test.swift
@@ -39,7 +39,7 @@ class mldivide_test: XCTestCase
 
     func testBasic() 
     {        
-        // least-squares solution of underdetermined system
+        // min norm solution of underdetermined system
         let A = Matrix<Double>([[1, 2, 0], [0, 4, 3]], name: "A")
         let B = Matrix<Double>([[8], [18]], name: "B")
         let x = A-/B              
@@ -49,5 +49,27 @@ class mldivide_test: XCTestCase
         print(x)
         print(Ax)
         XCTAssert(isequal(Ax, B, within: 0.1))
+
+        // find least squares of overdetermined system
+        let A2 = Matrix<Double>([[82,    64,   96],
+                                 [91,    10,   97],
+                                 [13,    28,   16],
+                                 [92,    55,   98]], name: "A2")
+        let B2 = Matrix<Double>([[96,    43],
+                                 [49,    92],
+                                 [81,    80],
+                                 [15,    96]], name: "B2")
+        let x2 = A2-/B2
+        let Ax2 = A2*x2
+        print(A2)
+        print(B2)
+        print(x2)
+        print(Ax2)
+
+        let ans = Matrix([[109.892,   57.9295],
+                          [28.526,    69.9968],
+                          [25.7719,   20.6471],
+                          [30.6735,   112.844]])
+        XCTAssert(isequal(Ax2, ans, within: 0.1), "\(Ax2) != \(ans)")        
     }
 }
