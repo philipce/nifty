bb1f1b92d993b9461945bac864e1585331e31c80
phil <philiperickson@gmail.com>
2017-02-10 23:59:40 -0700

    Add basic vector tests

--- Tests/NiftyTests/Vector_test.swift
+++ Tests/NiftyTests/Vector_test.swift
@@ -19,9 +19,33 @@ class Vector_test: XCTestCase
     {        
         print("\n\n")
 
-        let v = Vector([1.0, 345.35, 2342564.453, 354, 35345, 0.000234])
+        // array init and computed properties
+        let v = Vector([1.0, 345.35, 2342564.453, 354, 35345, 0.000234], name: "v")
         print("\(v)\n")
         print(v.csv)
+        XCTAssert(v.count == 6)
+        XCTAssert(v.data == [1.0, 345.35, 2342564.453, 354, 35345, 0.000234])
+        XCTAssert(v.name == "v")
+        XCTAssert(v.showName)
+        let _ = v.format
+
+        // repeated value init
+        var v2 = Vector(45, value: 1.0)
+        v2.showName = true
+        print(v2)
+        XCTAssert(v2.data.reduce(0.0,+) == 45.0)
+
+        // other vector init
+        var v3 = Vector(v2)
+        XCTAssert(isequal(v2, v3))
+
+        // subscript and slice
+        let x = v3[0...1]
+        XCTAssert(x.count == 2)
+        XCTAssert(x.data.reduce(0.0,+) == 2.0)
+        v3[0..<45] = Vector(45, value: 2.0)
+        XCTAssert(v3.data.reduce(0.0,+) == 90.0)
+
 
         print("\n\n")
     }
