75c4fef69acfc9170e4fca02ce3b68dbe7a3a055
Philip Erickson <philiperickson@gmail.com>
2018-07-24 20:36:25 -0600

    Genericize addition to all tensors of all numerics

--- Tests/NiftyTests/plus_test.swift
+++ Tests/NiftyTests/plus_test.swift
@@ -6,7 +6,7 @@
  *
  *  Author: Philip Erickson
  *  Creation Date: 22 Jan 2017
- *  Contributors: Adam Duracz
+ *  Contributors: Philip Erickson, Adam Duracz
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at
